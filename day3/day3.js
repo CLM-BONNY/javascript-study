7fee2ee65ef769692ee621fe69324119642ff555
minseon6371 <66056874+minseon6371@users.noreply.github.com>
2021-09-11 00:38:33 +0900

    Create day3.js

---
+++ day3/day3.js
@@ -0,0 +1,12 @@
+let p = document.getElementsByTagName('p');
+
+function changeText0() {
+    p[0].innerHTML = "KIM";
+}
+function changeText1() {
+    p[1].innerHTML = "MAN";
+}
+function changeText2() {
+    p[2].innerHTML = "DU";
+}
+
